03ffb35ba6d6401239ea05c1627f7dc94f6d4747
mayurphumatiya <mayurphumatiya13@gmail.com>
2023-03-22 18:16:01 +0530

    added routes and completed UI of homepage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,51 @@
-import React from 'react'
-import logo from "../assets/shared/logo.svg"
+import React from "react";
+import logo from "../assets/shared/logo.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className='primary-header'>
-        <div>
-            <img src={logo} alt="space tourism logo" className='logo'/>
-        </div>
-         <nav>
-                        <ul className="primary-navigation underline-indicators flex">
-                            <li className="active"><a className="ff-sans-cond uppercase text-white letter-spacing-2" href="#"><span>00</span>Home</a></li>
-                            <li><a className="ff-sans-cond uppercase text-white letter-spacing-2" href="#"><span>01</span>Destination</a></li>
-                            <li><a className="ff-sans-cond uppercase text-white letter-spacing-2" href="#"><span>02</span>Crew</a></li>
-                            <li><a className="ff-sans-cond uppercase text-white letter-spacing-2" href="#"><span>02</span>Technology</a></li>
-                        </ul>
-                    </nav>
+    <header className="primary-header">
+      <div>
+        <img src={logo} alt="space tourism logo" className="logo" />
+      </div>
+      <nav>
+        <ul className="primary-navigation underline-indicators flex">
+          <li className="active">
+            <Link
+              to="/"
+              className="ff-sans-cond uppercase text-white letter-spacing-2"
+            >
+              <span>00</span>Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/destination"
+              className="ff-sans-cond uppercase text-white letter-spacing-2"
+            >
+              <span>01</span>Destination
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/crew"
+              className="ff-sans-cond uppercase text-white letter-spacing-2"
+            >
+              <span>02</span>Crew
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/technology"
+              className="ff-sans-cond uppercase text-white letter-spacing-2"
+            >
+              <span>02</span>Technology
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
