e6b901851233fbb94dfa09cb5f87a70e0f051a04
mayurphumatiya <mayurphumatiya13@gmail.com>
2023-04-01 21:06:40 +0530

    login dialog stage setup

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../styles/Dialog.css"
+
+interface DialogProps{
+  setOpen: React.Dispatch<React.SetStateAction<Boolean>>,
+}
+
+const Dialog = (props:DialogProps) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Dialog
