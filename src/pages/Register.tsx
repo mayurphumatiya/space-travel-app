6f398d62374f6506988c6c88a0871d8d689a2d94
mayurphumatiya <mayurphumatiya13@gmail.com>
2023-04-19 18:05:41 +0530

    routes json and some UI Changes in Register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -28,40 +28,41 @@ const Register = () => {
           onSubmit={handleSubmit}
           className="form flex fs-500 ff-sans-cond letter-spacing-2"
         >
-            <div className="form-div flex" style={{ width:"100%"}}>
+          <div className="form-div flex" style={{ width: "100%" }}>
             <div className="form-align flex">
-            <label>First name:</label>
-            <input
-              type="text"
-              name="first_name"
-              value={register.first_name}
-              onChange={handleChange}
-            />
-            <label>Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={register.email}
-              onChange={handleChange}
-            />
+              <label>First name:</label>
+              <input
+                type="text"
+                name="first_name"
+                value={register.first_name}
+                onChange={handleChange}
+              />
+              <label>Last name:</label>
+              <input
+                type="text"
+                name="last_name"
+                value={register.last_name}
+                onChange={handleChange}
+              />
             </div>
             <div className="form-align flex">
-            <label>Last name:</label>
-            <input
-              type="text"
-              name="last_name"
-              value={register.last_name}
-              onChange={handleChange}
-            />
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              value={register.password}
-              onChange={handleChange}
-            />
+              <label>Email:</label>
+              <input
+                type="email"
+                name="email"
+                value={register.email}
+                onChange={handleChange}
+              />
+
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                value={register.password}
+                onChange={handleChange}
+              />
             </div>
-            </div>
+          </div>
           <button
             className="login-btn uppercase text-accent letter-spacing-2"
             type="submit"
