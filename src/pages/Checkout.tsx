e943b785bea5d5e1eb51963f0ba4b9a6195e02a9
mayurphumatiya <mayurphumatiya13@gmail.com>
2023-05-18 18:20:27 +0530

    Contact details checkout added

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { showNav } from "../Store/Slices/NavbarSlice";
+import "../styles/Checkout.css";
+import ContactUpdate from "../components/ContactUpdate";
 
 const Checkout = () => {
   const dispatch = useDispatch();
-
 
   useEffect(() => {
     dispatch(showNav(false));
@@ -12,26 +13,13 @@ const Checkout = () => {
 
   return (
     <>
-      <div>
-        <h1 className="uppercase fs-700 ff-serif letter-spacing-1 text-accent">Checkout</h1>
-        <form>
-            <div>
-            <label>First Name:</label>
-            <input type="text" name="first_name" />
-            </div>
-            
-            <div>
-            <label>Last Name:</label>
-            <input type="text" name="first_name" />
-            </div>
-            
-            <div>
-            <label>Email Id:</label>
-            <input type="text" name="first_name" />
-            </div>
-
-            
-        </form>
+      <div className="container">
+        <div>
+          <h1 className="main-heading uppercase fs-700 ff-serif letter-spacing-1 text-accent">
+            Checkout
+          </h1>
+        </div>
+        <ContactUpdate />
       </div>
     </>
   );
